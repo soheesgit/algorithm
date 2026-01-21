3e90151f5cc82c2e5ad0327daf739516515b526b
Moon So Hee <144293475+soheesgit@users.noreply.github.com>
2026-01-21 23:01:57 +0900

    [Bronze II] Title: 공약수, Time: 404 ms, Memory: 16232 KB -BaekjoonHub

---
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int A = 0;
+		int B = 0;
+		int C = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		if(n == 2) {
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			//두 수중 작은수까지만 반복하는 반복문
+			for(int i = 1; i <= Math.min(A, B); i++) {
+				if(A % i == 0 && B % i == 0) {
+					System.out.println(i);
+				}
+			}
+		}else {
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			//세 수중 작은수까지만 반복하는 반복문
+			for(int i = 1; i <= Math.min(A, Math.min(B, C)); i++) {
+				if(A % i == 0 && B % i == 0 && C % i == 0) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+
+}
