822c75e33eddac29b01eaa8749f86cdc0bee04d0
Moon So Hee <144293475+soheesgit@users.noreply.github.com>
2026-01-31 23:07:45 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 196 ms, Memory: 18080 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        //첫번째 식
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        // 두번째 식
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        //각각의 x, y가 -999 <= x, y <= 999이기 때문에
+        for(int i = -999; i <= 999; i++) { 
+            for(int j = -999; j <= 999; j++) {
+                if(a*i + b*j  == c){ // ax + by 가 C이고(x = i, y = j)
+                    if(d*i + e*j  == f) { // dx + ey가 f일 때(x = i, y = j)
+                        bw.write(i + " " + j + "\n");
+                        break;
+                    }
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
